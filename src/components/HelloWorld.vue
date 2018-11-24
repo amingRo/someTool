66dd28c3f4b89fc42b2694f9cedcc1f88ac2fbfc
郭先阅 <guoxianyue@megvii.com>
2018-11-24 16:37:32 +0800

    auto

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,60 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="menu">
+    <el-container>
+      <el-header>
+        <el-tabs v-model="activeName" @tab-click="handleClick" type="card">
+          <el-tab-pane label="描点" name="point">图片描点</el-tab-pane>
+          <el-tab-pane label="画框" name="frame">画框</el-tab-pane>
+          <el-tab-pane label="画线" name="line">画线</el-tab-pane>
+        </el-tabs>
+      </el-header>
+      <el-container>
+        <el-aside width="300px">
+          <span class="inputData">
+            <el-radio v-model="dataType" label="str">字符串</el-radio>
+            <el-radio v-model="dataType" label="num">数字</el-radio>
+          </span>
+          <span class="inputData">
+            <el-radio v-model="imgType" label="img">上传图片</el-radio>
+            <el-radio v-model="imgType" label="base64">base64</el-radio>
+          </span>
+        </el-aside>
+        <el-main>
+          <div class="childCo">
+            <el-input
+              v-if="dataType==='str'"
+              class="usefulData"
+              :placeholder='placeHoledr'
+              type="textarea"
+              :rows="4"
+            ></el-input>
+            <div
+              v-else
+              class="usefulData">
+              <el-button @click="addPoint" class="addPoint">新增</el-button>
+              <p v-for="val in pointData" :key="val.x" class="pointLine">
+                <span>X:</span><el-input v-model="val.x" class="inputNum"></el-input>
+                <span>Y:</span><el-input v-model="val.y" class="inputNum"></el-input>
+              </p>
+            </div>
+          </div>
+          <div class="childCo">
+            <el-input
+              v-if="imgType==='base64'"
+              class="usefulData"
+              placeholder='base64数据'
+              type="textarea"
+              :rows="4"
+            ></el-input>
+            <div
+              v-else
+              class="usefulData">
+              <input type="file" onchange="drawImg(this.files)" accept="image/*">
+            </div>
+          </div>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -88,26 +63,64 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      placeHoledr: '"x1,y1,x2,y2,x3,y3,x4,y4..."',
+      activeName: 'point',
+      dataType: 'str',
+      imgType: 'base64',
+      pointData: [{ x: '', y: '' }],
     };
+  },
+  methods: {
+    addPoint() {
+      if (this.pointData.length < 4) {
+        this.pointData.push({ x: '', y: '' });
+      }
+    },
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.menu{
+  width: 1000px;
+  padding: 20px 0;
+  margin: 20px auto;
+  background: #fff;
+  height: 100%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.fen{
+  height: 30px;
+
+  margin-bottom: 10px;
 }
-li {
+.usefulData{
   display: inline-block;
-  margin: 0 10px;
 }
-a {
-  color: #42b983;
+.inputData{
+  margin-left: 20px;
+  height: 150px;
+  display: inline-block;
+}
+.inputNum{
+  width: 100px;
+}
+.childCo{
+  min-height: 150px;
+}
+.pointLine{
+  display: inline-block;
+  margin-right: 30px;
+}
+.addPonit{
+  /* position: relative; */
+  float: left;
+}
+</style>
+<style>
+.el-main{
+  padding:0;
+}
+.el-header{
+  margin-bottom:50px;
 }
 </style>
