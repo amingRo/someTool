d806faa21f59958a5ffedca7660bc4487bd344ff
郭先阅 <guoxianyue@megvii.com>
2018-12-07 19:56:57 +0800

    auto

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,26 +27,6 @@
         </el-menu>
       </el-header>
       <el-container>
-        <!-- <el-aside width="300px">
-          <span class="inputData">
-            <span>描点坐标：</span>
-            <el-radio
-              v-model="dataType"
-              label="str">字符串</el-radio>
-            <el-radio
-              v-model="dataType"
-              label="num">数字</el-radio>
-          </span>
-          <span class="inputData">
-            <span>图片数据：</span>
-            <el-radio
-              v-model="imgType"
-              label="img">上传图片</el-radio>
-            <el-radio
-              v-model="imgType"
-              label="base64">base64</el-radio>
-          </span>
-        </el-aside> -->
         <el-main>
           <p class="inputData">
             <span>
@@ -62,7 +42,7 @@
                 @click="addPoint"
                 size="mini"
                 type="primary"
-                class="addPoint">新增</el-button>
+                class="addPoint">新增坐标</el-button>
             </span>
           </p>
           <div class="childCo">
@@ -77,8 +57,17 @@
             <div
               v-else
               class="usefulData">
-
-
+              <p
+                class="pointCon"
+                v-for="(point,index) in pointData"
+                :key="index">
+              <el-input :value="point.x">
+                <template slot="prepend">X</template>
+              </el-input>
+              <el-input :value="point.y">
+                <template slot="prepend">Y</template>
+              </el-input>
+              </p>
             </div>
           </div>
           <p class="inputData">
@@ -119,10 +108,26 @@
             <img :src="imgData">
           </div>
           <div class="buttonContainer">
-            <el-button>下载图片</el-button>
+            <p class="inputData2">
+              <span>画笔颜色:</span>
+              <el-color-picker v-model="penColor"></el-color-picker>
+            </p>
+            <p class="inputData2">
+              <span>画笔粗细:（单位：像素）
+                <el-input-number
+                  v-model="penWidth"
+                  size="small"
+                  :min="1"
+                  :max="5">
+                </el-input-number>
+              </span>
+            </p>
+            <p>
+              <el-button>下载图片</el-button>
             <el-button
               type="primary"
               @click="drawBegin">开始绘制</el-button>
+            </p>
           </div>
         </el-main>
       </el-container>
@@ -142,8 +147,8 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      activeIndex1: '1',
-      icon: {
+      activeIndex1: '1', // 当前的绘制模式
+      icon: { // 头部选项卡icon
         frame,
         frameSelected,
         line,
@@ -151,32 +156,63 @@ export default {
         point,
         pointSelected
       },
-      imgData: '',
+      imgData: '', // 生成的图片
       placeHoledr: '"x1,y1,x2,y2,x3,y3,x4,y4..."',
-      activeName: 'point',
-      dataType: 'str',
-      imgType: 'base64',
-      fileList: [],
-      pointData: [{ x: '', y: '' }],
-      pointStr: '',
-      imgBase64: '',
-      img: null,
+      dataType: 'str', // 数据输入类型
+      imgType: 'base64', // 图片类型
+      pointData: [{ x: '', y: '' }], // 点坐标数据
+      pointStr: '', // 点坐标数据
+      imgBase64: '', // 图片的base64
+      img: null, // 上传的图片数据
+      penColor: 'red', // 画笔颜色
+      penWidth: 1// 画笔粗细
     };
   },
   methods: {
+    /**
+     * 点击头部绘制模式选项卡.
+     * @param {*} index - 不同模式的索引值.
+     * @returns.
+     */
     handleSelect(index) {
       this.activeIndex1 = index;
-    },
-    saveImg(data) {
-      console.log(data);
-      this.img = data;
-    },
-    addPoint() {
-      if (this.pointData.length < 4) {
-        this.pointData.push({ x: '', y: '' });
+      if (index === '2') {
+        if (this.pointData.length > 2) {
+          this.pointData.splice(2, this.pointData.length - 2);
+        }
       }
     },
-    handleClick() {},
+    /**
+     * 图片上传成功后保存图片数据.
+     * @param {*} data - 图片数据.
+     * @returns.
+     */
+    saveImg(data) {
+      this.img = data;
+    },
+    /**
+     * 增加数据录入框.
+     * @param.
+     * @returns.
+     */
+    addPoint() {
+      if (this.activeIndex1 === '2') {
+        if (this.pointData.length < 2) {
+          this.pointData.push({ x: '', y: '' });
+        } else {
+          this.$message.warning('画线最多输入2个点！');
+        }
+      } else if (this.pointData.length < 4) {
+        this.pointData.push({ x: '', y: '' });
+      } else {
+        this.$message.warning('最多输入4个点！');
+      }
+    },
+    /**
+     * 开始绘制.
+     * @param {*} index - 不同模式的索引值.
+     * @returns.
+     */
     drawBegin() {
       const canvas = document.createElement('canvas');
       const ctx = canvas.getContext('2d');
@@ -185,7 +221,6 @@ export default {
       if (this.dataType === 'str') {
         pointData = this.pointStr.split(',');
       } else {
-        console.log(this.pointData);
         this.pointData.forEach((value) => {
           pointData.push(value.x);
           pointData.push(value.y);
@@ -196,16 +231,38 @@ export default {
         canvas.height = img.height;
         ctx.drawImage(img, 0, 0, img.width, img.height);
         pointData.forEach((value, index) => {
-          if (index % 2 === 0) {
-            ctx.beginPath();
-            ctx.arc(Number(pointData[index]), Number(pointData[index + 1]), 5, 0, 2 * Math.PI, true);
-            ctx.fillStyle = 'red';
-            ctx.fill();
+          if (this.activeIndex1 === '1') {
+            if (index % 2 === 0) {
+              ctx.beginPath();
+              ctx.arc(Number(pointData[index]), Number(pointData[index + 1]), 5, 0, 2 * Math.PI, true);
+              ctx.fillStyle = 'red';
+              ctx.fill();
+            }
+          } else if (this.activeIndex1 === '2') {
+            if (index % 4 === 0) {
+              ctx.beginPath();
+              ctx.strokeStyle = 'red';
+              ctx.lineWidth = 5;
+              ctx.moveTo(pointData[index], pointData[index + 1]);
+              ctx.lineTo(pointData[index + 2], pointData[index + 3]);
+              ctx.closePath();
+              ctx.stroke();
+            }
+          } else if (this.activeIndex1 === '3') {
+            if (index % 4 === 0) {
+              ctx.beginPath();
+              ctx.strokeStyle = 'red';
+              ctx.lineWidth = 5;
+              ctx.moveTo(pointData[index], pointData[index + 1]);
+              ctx.lineTo(pointData[index + 2], pointData[index + 3]);
+              ctx.closePath();
+              ctx.stroke();
+            }
           }
         });
-        this.$refs.imgContainer.innerHTML = `<img src=${canvas.toDataURL('image/png', 1)}>`;
+        this.imgData = canvas.toDataURL('image/png', 1);
+        // this.$refs.imgContainer.innerHTML = `<img src=${canvas.toDataURL('image/png', 1)}>`;
       };
-
 
       if (this.imgType === 'base64') {
         img.src = this.imgBase64;
@@ -237,6 +294,12 @@ export default {
   line-height: 30px;
   margin: 10px 0px;
 }
+.inputData2{
+  display: flex;
+  justify-content: flex-start;
+  width: 90%;
+  line-height: 50px;
+}
 .childCo{
   height: 100px;
   background: #F9F9FA;
@@ -255,12 +318,18 @@ export default {
   display: inline-flex;
   justify-content: flex-end;
   align-items: flex-end;
+  flex-direction: column;
 }
 .usefulData{
   text-align: left;
 }
 .addPoint{
   margin-left: 20px;
+}
+.pointCon{
+  width: 200px;
+  float: left;
+  margin: 7px 15px;
 }
 </style>
 <style>
