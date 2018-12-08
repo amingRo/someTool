79ed6e7b21d10de439ab08bfc2f0ba8b6f5c0dea
郭先阅 <guoxianyue@megvii.com>
2018-12-08 16:47:17 +0800

    optimizate webpack

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import ElementUI from 'element-ui';
+import { Button, MenuItem, Container, Header, Menu, Main, Radio, Input, ColorPicker, InputNumber, Loading, Message } from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App';
 
-Vue.use(ElementUI);
+Vue.use(Button);
+Vue.use(MenuItem);
+Vue.use(Container);
+Vue.use(Header);
+Vue.use(Menu);
+Vue.use(Main);
+Vue.use(Radio);
+Vue.use(Input);
+Vue.use(ColorPicker);
+Vue.use(InputNumber);
+
+Vue.prototype.$loading = Loading.service;
+Vue.prototype.$message = Message;
+
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
